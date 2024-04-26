64638a26adec099d9ff596b4ed7360360a7640ff
Petar Peychev <petar.peychev@theverygroup.com>
2024-04-26 15:05:55 +0100

    changr base url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { getBlogPosts } from 'app/blog/utils'
 
-export const baseUrl = 'https://petarpeychev.com'
+export const baseUrl = 'https://petar.cloud'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
